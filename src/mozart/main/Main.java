aa83b70d8dccaba8432fbd27564dd0a6790befb2
smk37git <sdmain3@gmail.com>
2025-04-03 15:11:30 -0500

    Added csv file into project and implemented first steps in Main.java (Error Handling)

--- src/mozart/main/Main.java
+++ src/mozart/main/Main.java
@@ -7,16 +7,22 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String path = "C:\\Users\\gregg\\Downloads\\mystery_song.csv";
-		ArrayList<MidiEventData> events = MidiCsvParser.parsedData(path);
-		for (int i = 0; i < 100; i++) {
-			System.out.println(events.get(i).toString());
+		try {
+			// TODO Auto-generated method stub
+			String path = "C:\\Users\\sdmai\\Downloads\\mystery_song.csv";
+			ArrayList<MidiEventData> events = MidiCsvParser.parsedData(path);
+			for (int i = 0; i < 100; i++) {
+				System.out.println(events.get(i).toString());
+			}
+			
+			/*notes: only using .toString for testing, not needed in general.
+			 * noteOnOff == 1 means note ON, noteOnOff == 0 means note OFF
+			 * remember to download the .csv file and set path to its location on your pc
+			*/
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		/*notes: only using .toString for testing, not needed in general.
-		 * noteOnOff == 1 means note ON, noteOnOff == 0 means note OFF
-		 * remember to download the .csv file and set path to its location on your pc
-		*/
+		
 	}
 
 }
