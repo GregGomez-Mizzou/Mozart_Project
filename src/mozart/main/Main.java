021481574fb4eb9fc351d3a1ff4fba1f8d2ef473
smk37git <sdmain3@gmail.com>
2025-04-03 15:16:56 -0500

    Create Objects on run

--- src/mozart/main/Main.java
+++ src/mozart/main/Main.java
@@ -1,7 +1,11 @@
 package mozart.main;
 import mozart.eventdata.MidiEventData;
+import mozart.midiEventFactory.MidiEventFactory;
 import mozart.parser.MidiCsvParser;
 import java.util.ArrayList;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Track;
+import mozart.abstractFactory.*;
 
 
 public class Main {
@@ -11,14 +15,32 @@ public class Main {
 			// TODO Auto-generated method stub
 			String path = "C:\\Users\\sdmai\\Downloads\\mystery_song.csv";
 			ArrayList<MidiEventData> events = MidiCsvParser.parsedData(path);
+			
+			Sequence sequence = new Sequence(Sequence.PPQ, 384);
+			Track track = sequence.createTrack();
+			
+			MidiEventAbstractFactory factoryAbstract = new StandardMidiEventFactoryAbstract();
+			// Other Factories below:
+			
+			MidiEventFactory factory = factoryAbstract.createFactory();
+			
+			// Choose Instrument Strategy
+			
+			
+			
+			
+			
+			
+			/*
 			for (int i = 0; i < 100; i++) {
 				System.out.println(events.get(i).toString());
 			}
 			
-			/*notes: only using .toString for testing, not needed in general.
+			notes: only using .toString for testing, not needed in general.
 			 * noteOnOff == 1 means note ON, noteOnOff == 0 means note OFF
 			 * remember to download the .csv file and set path to its location on your pc
 			*/
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
